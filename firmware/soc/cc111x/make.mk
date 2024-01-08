aadd3a48653a62be4c3cf6f7c030d6391592fda0
Skip Hansen <skip@gfrn.org>
2024-01-08 14:54:18 -0800

    Remove SOURCES from soc/cc111x/make.mk, include them in toplevel Makefile instead.

--- firmware/soc/cc111x/make.mk
+++ firmware/soc/cc111x/make.mk
@@ -2,10 +2,6 @@
 CPU = 8051
 
 FLAGS += --xram-loc 0xf000 --xram-size 0xda2 --model-medium
-SOURCES += $(SOC_DIR)/wdt.c $(SOC_DIR)/timer.c $(SOC_DIR)/sleep.c 
-SOURCES += $(SOC_DIR)/adc.c $(SOC_DIR)/radio.c $(SOC_DIR)/aes.c 
-SOURCES += $(SOC_DIR)/soc.c $(SOC_DIR)//printf.c
-
 
 test: main.bin
 	sudo cc-tool -e -w $^ -f
